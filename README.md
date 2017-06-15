ca827d3460ce5fce03c80fa9283a243cbb1d75dc
Gregg Tavares <github@greggman.com>
2017-06-15 10:44:05 +0900

    update for no-electron

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Simple
-======
+Simple-No-Electron
+==================
 
 This is a sample game for the [HappyFunTimes party games system](http://greggman.github.io/HappyFunTimes).
 
@@ -7,11 +7,29 @@ This is a sample game for the [HappyFunTimes party games system](http://greggman
 
 It shows using the screen as a touch surface.
 
-Cloning
--------
+This one does not use electron. To run, follow these steps
 
-[If you want to clone this follow the instructions here](http://docs.happyfuntimes.net/docs/makinggames.html)
+1.  install [node.js](https://nodejs.org)
+2.  clone this repo
 
+       git clone https://github.com/greggman/hft-simple-no-electron.git
+
+3.  install dependencies
+
+       cd hft-simple-no-electron
+       npm install
+
+4.  run it
+
+       npm start
+
+5.  Open your browser to [`http://localhost:18679/game.html`](http://localhost:18679/game.html)
+6.  Open another browser to [`http://localhost:18679`](http://localhost:18679)
+7.  Get out your smartphone, make sure it's **on the same wifi** as your computer. Go to [`happyfuntimes.net`](http://happyfuntimes.net).
+
+    note: corp networks, libraries, coffee shop wifi will probably **not** work.
+
+See docs [http://docs.happyfuntimes.net/docs].
 
 
 
--- scripts/game.js
+++ scripts/game.js
@@ -30,131 +30,113 @@
  */
 "use strict";
 
-const isDevMode = process.env.NODE_ENV === 'development';
-const requirejs = require('requirejs');
-requirejs.config({
-  nodeRequire: require,
-  baseUrl: __dirname,
-});
+var GameServer = HFT.GameServer;
+var GameSupport = gameUtils.gameSupport;
+var Misc = sampleUI.misc;
+var PlayerNameManager = sampleUI.PlayerNameManager;
 
-// Start the main app logic.
-requirejs([
-    'happyfuntimes',
-    'hft-game-utils',
-    'hft-sample-ui',
-  ], function(
-    happyfuntimes,
-    gameUtils,
-    sampleUI) {
+var statusElem = document.getElementById("status");
+var canvas = document.getElementById("playfield");
+var ctx = canvas.getContext("2d");
+var players = [];
+var globals = {
+  itemSize: 15,
+};
+Misc.applyUrlSettings(globals);
 
-  var GameServer = happyfuntimes.GameServer;
-  var GameSupport = gameUtils.gameSupport;
-  var Misc = sampleUI.misc;
-  var PlayerNameManager = sampleUI.PlayerNameManager;
-
-  var statusElem = document.getElementById("status");
-  var canvas = document.getElementById("playfield");
-  var ctx = canvas.getContext("2d");
-  var players = [];
-  var globals = {
-    itemSize: 15,
+var pickRandomPosition = function() {
+  return {
+    x: 30 + Misc.randInt(canvas.width  - 60),
+    y: 30 + Misc.randInt(canvas.height - 60),
   };
-  Misc.applyUrlSettings(globals);
+};
 
-  var pickRandomPosition = function() {
-    return {
-      x: 30 + Misc.randInt(canvas.width  - 60),
-      y: 30 + Misc.randInt(canvas.height - 60),
-    };
-  };
+var Goal = function() {
+    this.pickGoal();
+    this.radiusesSquared = globals.itemSize * 2 * globals.itemSize;
+};
 
-  var Goal = function() {
-      this.pickGoal();
-      this.radiusesSquared = globals.itemSize * 2 * globals.itemSize;
-  };
+Goal.prototype.pickGoal = function() {
+  this.position = pickRandomPosition();
+};
 
-  Goal.prototype.pickGoal = function() {
-    this.position = pickRandomPosition();
-  };
+Goal.prototype.hit = function(otherPosition) {
+  var dx = otherPosition.x - this.position.x;
+  var dy = otherPosition.y - this.position.y;
+  return dx * dx + dy * dy < this.radiusesSquared;
+};
 
-  Goal.prototype.hit = function(otherPosition) {
-    var dx = otherPosition.x - this.position.x;
-    var dy = otherPosition.y - this.position.y;
-    return dx * dx + dy * dy < this.radiusesSquared;
-  };
+var Player = function(netPlayer, name) {
+  this.netPlayer = netPlayer;
+  this.name = name;
+  this.position = pickRandomPosition();
+  this.color = "green";
 
-  var Player = function(netPlayer, name) {
-    this.netPlayer = netPlayer;
-    this.name = name;
-    this.position = pickRandomPosition();
-    this.color = "green";
+  netPlayer.addEventListener('disconnect', Player.prototype.disconnect.bind(this));
+  netPlayer.addEventListener('move', Player.prototype.movePlayer.bind(this));
+  netPlayer.addEventListener('color', Player.prototype.setColor.bind(this));
 
-    netPlayer.addEventListener('disconnect', Player.prototype.disconnect.bind(this));
-    netPlayer.addEventListener('move', Player.prototype.movePlayer.bind(this));
-    netPlayer.addEventListener('color', Player.prototype.setColor.bind(this));
+  this.playerNameManager = new PlayerNameManager(netPlayer);
+  this.playerNameManager.on('setName', Player.prototype.handleNameMsg.bind(this));
+};
 
-    this.playerNameManager = new PlayerNameManager(netPlayer);
-    this.playerNameManager.on('setName', Player.prototype.handleNameMsg.bind(this));
-  };
-
-  // The player disconnected.
-  Player.prototype.disconnect = function() {
-    for (var ii = 0; ii < players.length; ++ii) {
-      var player = players[ii];
-      if (player === this) {
-        players.splice(ii, 1);
-        return;
-      }
+// The player disconnected.
+Player.prototype.disconnect = function() {
+  for (var ii = 0; ii < players.length; ++ii) {
+    var player = players[ii];
+    if (player === this) {
+      players.splice(ii, 1);
+      return;
     }
-  };
+  }
+};
 
-  Player.prototype.movePlayer = function(cmd) {
-    this.position.x = Math.floor(cmd.x * canvas.clientWidth);
-    this.position.y = Math.floor(cmd.y * canvas.clientHeight);
-    if (goal.hit(this.position)) {
-      // This will generate a 'scored' event on the client (player's smartphone)
-      // that corresponds to this player.
-      this.netPlayer.sendCmd('scored', {
-        points: 5 + Misc.randInt(6), // 5 to 10 points
-      });
-      goal.pickGoal();
-    }
-  };
-
-  Player.prototype.handleNameMsg = function(name) {
-    this.name = name;
-  };
-
-  Player.prototype.setColor = function(cmd) {
-    this.color = cmd.color;
-  };
-
-  var server = new GameServer();
-  GameSupport.init(server, globals);
-
-  var goal = new Goal();
-
-  // A new player has arrived.
-  server.addEventListener('playerconnect', function(netPlayer, name) {
-    players.push(new Player(netPlayer, name));
-  });
-
-  var drawItem = function(position, color) {
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(position.x, position.y, globals.itemSize, 0, Math.PI * 2);
-    ctx.fill();
-  };
-
-  var render = function() {
-    Misc.resize(canvas);
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    players.forEach(function(player) {
-      drawItem(player.position, player.color);
+Player.prototype.movePlayer = function(cmd) {
+  this.position.x = Math.floor(cmd.x * canvas.clientWidth);
+  this.position.y = Math.floor(cmd.y * canvas.clientHeight);
+  if (goal.hit(this.position)) {
+    // This will generate a 'scored' event on the client (player's smartphone)
+    // that corresponds to this player.
+    this.netPlayer.sendCmd('scored', {
+      points: 5 + Misc.randInt(6), // 5 to 10 points
     });
-    drawItem(goal.position, (globals.frameCount & 4) ? "red" : "pink");
-  };
-  GameSupport.run(globals, render);
+    goal.pickGoal();
+  }
+};
+
+Player.prototype.handleNameMsg = function(name) {
+  this.name = name;
+};
+
+Player.prototype.setColor = function(cmd) {
+  this.color = cmd.color;
+};
+
+var server = new GameServer();
+GameSupport.init(server, globals);
+
+var goal = new Goal();
+
+// A new player has arrived.
+server.addEventListener('playerconnect', function(netPlayer, name) {
+  players.push(new Player(netPlayer, name));
 });
+
+var drawItem = function(position, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(position.x, position.y, globals.itemSize, 0, Math.PI * 2);
+  ctx.fill();
+};
+
+var render = function() {
+  Misc.resize(canvas);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  players.forEach(function(player) {
+    drawItem(player.position, player.color);
+  });
+  drawItem(goal.position, (globals.frameCount & 4) ? "red" : "pink");
+};
+GameSupport.run(globals, render);
 
 
